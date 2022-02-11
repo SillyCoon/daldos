5b438144a3be114ab1f39dd756f8eb065694f9dd
SillyCoon <ahtunget@gmail.com>
2022-02-11 12:47:21 +0300

    remove unused

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,26 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './game.css';
 import { GameModeEnum } from '../game/models/game-elements/enums/game-mode';
 import { Opponent } from '../game/logic/opponent/opponent';
-import { CanvasDrawer } from '../game/logic/drawer';
-import { InteractiveBoard } from '../game/logic/control/Interactive-board';
 import { Size } from '../game/models/draw/size';
-import { ColorScheme } from '../game/models/draw/color-scheme';
 import { Board } from './Board';
 import { Controls } from './Controls';
 import { Logger } from './Logger';
 import { GameState } from '../game/logic/game-state';
-import { Command } from '../game/logic/commands/command';
-import { Color } from '../game/models/game-elements/color';
 import styled from 'styled-components';
 import { CommandTypeEnum } from '../game/models/game-elements/enums/command-type';
 import { Coordinate } from '../game/models/game-elements/coordinate';
 import { Statistic } from '../game/models/game-elements/statistic';
 
-const colorScheme = new ColorScheme();
 const size = new Size();
-const board = new InteractiveBoard(size);
-const drawer = new CanvasDrawer(board, colorScheme, size);
 
 const GameWrapper = styled.div`
   display: flex;
