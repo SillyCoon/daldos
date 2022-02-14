5782e0c762ab1cca320a77df2e303c2ad482e0f8
SillyCoon <ahtunget@gmail.com>
2022-02-14 09:17:50 +0300

    pretty buttons

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,15 +1,39 @@
+import styled from 'styled-components';
+
 interface ControlsProps {
   onRoll: () => void;
   disabled: boolean;
 }
 
+const RollButton = styled.button`
+  width: 60px;
+  height: 60px;
+  background: brown;
+  color: white;
+  border: none;
+  :disabled {
+    background: #e6d9d9;
+  }
+`;
+
+const UndoButton = styled.button`
+  width: 60px;
+  height: 60px;
+  background: #d81010;
+  color: white;
+  border: none;
+  :disabled {
+    background: #e6d9d9;
+  }
+`;
+
 export const Controls = ({ onRoll, disabled }: ControlsProps) => {
   return (
     <div>
-      <button onClick={() => onRoll()} disabled={disabled}>
+      <RollButton onClick={() => onRoll()} disabled={disabled}>
         Roll
-      </button>
-      <button disabled={disabled}>Undo</button>
+      </RollButton>
+      <UndoButton disabled={disabled}>Undo</UndoButton>
     </div>
   );
 };
