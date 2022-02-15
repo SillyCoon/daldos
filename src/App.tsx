2b63512ba80972e823122096f6d89e13383dae34
SillyCoon <ahtunget@gmail.com>
2022-02-15 20:37:03 +0300

    some sort of registration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Game } from './components/Game';
-import { GameTypeSelector } from './components/GameTypeSelector';
+import { WelcomeScreen } from './components/WelcomeScreen';
 import { SimpleAI } from './logic/simple-ai';
 import { GameMode } from './model/enums/game-mode';
+import { io } from 'socket.io-client';
+import { Player } from './model/player';
 
 const Navbar = styled.div`
   width: 100%;
@@ -12,18 +14,36 @@ const Navbar = styled.div`
   margin-bottom: 10px;
 `;
 
+const ws = io('http://localhost:3000');
+
 function App() {
   const [gameMode, setGameMode] = useState<GameMode | null>(null);
+  const [player, setPlayer] = useState<Player | null>(null);
 
-  const handleAIClick = () => {
-    setGameMode(GameMode.AI);
+  const handleWelcomeScreenSubmit = (player: Player, mode: GameMode) => {
+    setGameMode(mode);
+    setPlayer(player);
   };
 
-  const handleSingleClick = () => {
-    setGameMode(GameMode.Single);
-  };
+  useEffect(() => {
+    fetch('http://localhost:3000/players', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(player),
+    }).then((r) => r.json().then((t) => console.log(t)));
+  });
 
   const needOpponent = gameMode !== GameMode.Single;
+
+  // useEffect(() => {
+  //   ws.on('connect', () => {
+  //     console.log('connected');
+  //     ws.send(JSON.stringify({ type: 'events', msg: 'string' }));
+  //   });
+  // });
 
   const renderGame = () =>
     gameMode ? (
@@ -33,10 +53,9 @@ function App() {
         opponent={needOpponent ? new SimpleAI() : undefined}
       ></Game>
     ) : (
-      <GameTypeSelector
-        onAIClick={handleAIClick}
-        onSingleClick={handleSingleClick}
-      ></GameTypeSelector>
+      <WelcomeScreen
+        onWelcomeScreenSubmit={handleWelcomeScreenSubmit}
+      ></WelcomeScreen>
     );
 
   return <div className="App">{renderGame()}</div>;
---
+++ src/components/WelcomeScreen.tsx
@@ -0,0 +1,61 @@
+import { useFormik } from 'formik';
+import { GameMode } from '../model/enums/game-mode';
+import { Player } from '../model/player';
+
+export interface GameTypeSelectorProps {
+  onWelcomeScreenSubmit: (name: Player, mode: GameMode) => void;
+}
+
+export const WelcomeScreen = ({
+  onWelcomeScreenSubmit: onGameModeChange,
+}: GameTypeSelectorProps) => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      mode: null,
+    },
+    onSubmit: ({ name, mode }) => {
+      onGameModeChange({ name }, mode ?? GameMode.Single);
+    },
+    validate: ({ name, mode }) =>
+      !name || !mode ? { errors: 'not selected' } : {},
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>
+        Your Name:
+        <input
+          id="name"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+      </label>
+      <h1>Select game type:</h1>
+      <div id="mode-selector">
+        <label>
+          Single
+          <input
+            type="radio"
+            name="mode"
+            value={GameMode.Single}
+            onChange={formik.handleChange}
+          ></input>
+        </label>
+        <label>
+          AI
+          <input
+            type="radio"
+            name="mode"
+            value={GameMode.AI}
+            onChange={formik.handleChange}
+          ></input>
+        </label>
+      </div>
+      <button type="submit" disabled={!(formik.isValid && formik.dirty)}>
+        Play!
+      </button>
+    </form>
+  );
+};
